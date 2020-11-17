139ce98ba7645e927dc99a7bf052d017423476e3
James Downs <jamesdownslab@gmail.com>
2020-11-17 12:15:30 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "r") as fh:
+    license = fh.read()
+
+setuptools.setup(
+    name='qwidgets',
+    version='0.1',
+    license=license,
+    packages=setuptools.find_packages(
+        exclude=('tests', 'docs')
+    ),
+    url='https://github.com/MikeSmithLabTeam/labvision',
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'pyside2'
+    ],
+    include_package_data=True,
+)
