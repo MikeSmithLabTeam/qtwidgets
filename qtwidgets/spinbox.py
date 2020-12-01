91bacf59488d036acba79c0d1b1cfa81ac9c1279
James <jamesdownslab@gmail.com>
2020-12-01 15:01:57 +0000

    Added documentation and type hints to spinbox.py

--- qtwidgets/spinbox.py
+++ qtwidgets/spinbox.py
@@ -1,16 +1,26 @@
-from PyQt5.QtWidgets import QSpinBox
+from PyQt5.QtWidgets import QSpinBox, QWidget
 from PyQt5.QtCore import pyqtSignal
 
 
 class QSteppedSpinBox(QSpinBox):
+    """
+    Child of QSpinBox that limits the selectable values by changing the
+    range and singleStep of the spinbox.
+
+    The signal that requires a slot is 'onValueChanged'.
+
+    The 'valueChanged' signal will return the wrong values.
+    """
     onValueChanged = pyqtSignal(int)
 
-    def __init__(self, parent=None):
+    def __init__(self,
+                 parent: QWidget = None
+                 ):
         QSpinBox.__init__(self, parent)
         self.valueChanged.connect(self.sliderValueChanged)
         self.setKeyboardTracking(False)
 
-    def sliderValueChanged(self, i):
+    def sliderValueChanged(self, i: int) -> None:
         if (i - self.minimum()) % self.singleStep() == 0:
             self.onValueChanged.emit(i)
         else:
