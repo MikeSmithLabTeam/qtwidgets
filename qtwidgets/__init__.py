1718c3a7dd67d7b271cd7fd2cc9b0de8f16cb703
mikesmithlab <mikesmithlab@gmail.com>
2021-01-28 17:05:48 +0000

    Added imports to __init__

--- qtwidgets/__init__.py
+++ qtwidgets/__init__.py
@@ -2,3 +2,4 @@ from .data import data_dir
 from .images import QImageViewer
 from .sliders import QCustomSlider, QSteppedSlider
 from .spinbox import QSteppedSpinBox
+from .textbox import QCustomTextBox
