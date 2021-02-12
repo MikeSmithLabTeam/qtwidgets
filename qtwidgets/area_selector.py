beb8469178b26df2ff6be3c0aa44508786486a00
mikesmithlab <mikesmithlab@gmail.com>
2021-02-12 15:51:18 +0000

    Select Area now can be adjusted afterwards.

--- qtwidgets/area_selector.py
+++ qtwidgets/area_selector.py
@@ -1,13 +1,22 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import numpy as np
 
 
 class SelectAreaWidget(QWidget):
-    def __init__(self, shape=None, geometry=None, colour=QColor(250, 10, 10, 80)):
-        self.shape=shape
-        self.points = []
-        self.display_point_list = []
+    def __init__(self, shape=None, viewer=None, points=None, colour=QColor(250, 10, 10, 80), handle_rad=25):
+        self.viewer = viewer
+        self.viewer.keyPressed.connect(self.keyPressEvent)
+        geometry = self.viewer.geometry
+  
+        self.shape=shape 
+        self.handle_rad = handle_rad      
+        self.points = points # Store coords        
+        self.handles = [] # Contains the QEllipse used to drag shapes around
+        self.adjustable = (points is not None)
+
+        #self.display_point_list = []
         self.colour=colour
         super().__init__()
         self.setGeometry(geometry)
@@ -15,36 +24,128 @@ class SelectAreaWidget(QWidget):
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.transparent)
         self.setPalette(p)
-        self.begin = QPoint()
-        self.end = QPoint()
+
+        self.parse_points(points)
+        if points is not None:
+            self.draw_shape()
+            self.update()
+
+    def parse_points(self, points):
+        self.qpoints=[]
+        if points is not None: 
+            for point in points:
+                self.qpoints.append(QPoint(point[0],point[1]))
 
     def paintEvent(self, event):
+        self.draw_shape()
+
+    def draw_shape(self):
+        if len(self.qpoints) > 0:
+            self.handles = []
+            if self.shape=='rect':
+                self.rectangle(self.qpoints)
+            elif self.shape == 'ellipse':
+                self.ellipse(self.qpoints)
+            elif self.shape == 'circle':
+                self.circle(self.qpoints)       
+            elif self.shape == 'polygon':
+                self.polygon(self.qpoints)
+
+    def rectangle(self, qpoints):
         qp = QPainter(self)
         br = QBrush(self.colour)
         qp.setBrush(br)
+        qp.drawRect(QRect(qpoints[0], qpoints[1]))   
+        handles = [QPoint(qpoints[0].x(),qpoints[0].y()),
+                   QPoint(qpoints[1].x(),qpoints[1].y())]
+        self.add_handles(qp, handles)
+        self.qpoints = qpoints 
+        self.points = [(qpoints[0].x(),qpoints[0].y()),(qpoints[1].x(),qpoints[1].y())]
 
-        if self.shape == 'rect':
-            qp.drawRect(QRect(self.begin, self.end))
-        elif self.shape == 'ellipse':
-            qp.drawEllipse(QRect(self.begin, self.end))
-            self.ellipse_pts = ((self.begin.x(),self.begin.y()),(self.end.x(),self.end.y()))
-        elif self.shape == 'polygon':
-            points=QPolygon(self.points)
-            qp.drawPolygon(points)
+    def ellipse(self, qpoints):
+        qp = QPainter(self)
+        br = QBrush(self.colour)
+        qp.setBrush(br)
+        qp.drawEllipse(QRect(qpoints[0], qpoints[1]))  
+        handles = [QPoint(qpoints[0].x(),qpoints[0].y()),
+                   QPoint(qpoints[1].x(),qpoints[1].y())]
+        self.add_handles(qp, handles)
+        self.qpoints=qpoints
+        self.points = [(qpoints[0].x(),qpoints[0].y()),(qpoints[1].x(),qpoints[1].y())]
+        
+    def circle(self, qpoints):
+        qp = QPainter(self)
+        br = QBrush(self.colour)
+        qp.setBrush(br)
+        dx = qpoints[1].x() - qpoints[0].x()
+        dy = qpoints[1].y() - qpoints[0].y()
+        rad = ((dx)**2 + (dy)**2)**0.5
+        qp.drawEllipse(qpoints[0], rad, rad)
+        self.qpoints=qpoints
+        handles = self.qpoints
+        self.add_handles(qp, handles)
+        self.points = [(qpoints[0].x(),qpoints[0].y()),(qpoints[1].x(),qpoints[1].y())]
+
+    def polygon(self, qpoints):
+        qp = QPainter(self)
+        br = QBrush(self.colour)
+        qp.setBrush(br)
+        polygon = QPolygon(qpoints)
+        qp.drawPolygon(polygon) 
+        self.add_handles(qp, qpoints)  
+        self.points=[]   
+        for qpoint in qpoints:
+            self.points.append((qpoint.x(),qpoint.y()))
+        
+
+    def add_handles(self, qp, handles, colour=QColor(Qt.lightGray)):
+        br2=QBrush(colour)
+        qp.setBrush(br2)
+        if len(handles)>0:
+            for qpoint in handles:
+                qp.drawEllipse(qpoint, self.handle_rad, self.handle_rad)
+                self.handles.append(qpoint)
 
     def mousePressEvent(self, event):
         self.begin = event.pos()
         self.end = event.pos()
-        if self.shape == 'polygon':
-            self.points.append(QPoint(self.begin.x(),self.begin.y()))
-            self.display_point_list.append((self.begin.x(),self.begin.y()))
-        else:
-            self.update()
+        
+        if (self.shape == 'polygon') &  (self.adjustable == False):
+            self.qpoints.append(self.begin)
+        elif self.adjustable:
+            #This enables updating the position of handle and shape
+            self.handle_index = self.which_handle()  
+        self.update()
 
     def mouseMoveEvent(self, event):
         self.end = event.pos()
+        if self.adjustable: 
+            if self.handle_index is not None:               
+                self.points[self.handle_index] = (self.end.x(),self.end.y())
+                self.qpoints[self.handle_index] = self.end
+        else:
+            if self.shape != 'polygon':
+                self.points = [(self.begin.x(),self.begin.y()),(self.end.x(),self.end.y())]
+                self.qpoints = [self.begin, self.end]
         self.update()
-
+        
     def mouseReleaseEvent(self, event):
-        self.end = event.pos()
-        self.update()
+        if self.shape != 'polygon':
+            self.adjustable = True
+
+    def which_handle(self):
+        val = None
+        for index, handle in enumerate(self.qpoints):
+            distance2 = ((handle.x() - self.end.x())**2 + (handle.y() - self.end.y())**2)
+            if distance2 <= (self.handle_rad**2):#If they click within the handle
+                val=index
+        return val
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return:
+            self.adjustable = True
+            print(self.points)
+            
+        
+
+    
