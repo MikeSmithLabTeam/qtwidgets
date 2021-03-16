900ed774416637496c874db18ac91b82ad178189
mikesmithlab <mikesmithlab@gmail.com>
2021-03-16 10:17:29 +0000

    Added QCustomSLiderDecimal to init

--- qtwidgets/__init__.py
+++ qtwidgets/__init__.py
@@ -1,6 +1,6 @@
 from .data import data_dir
 from .images import QImageViewer
-from .sliders import QCustomSlider, QSteppedSlider
+from .sliders import QCustomSlider, QCustomSliderDecimal, QSteppedSlider
 from .spinbox import QSteppedSpinBox
 from .textbox import QCustomTextBox
 from .area_selector import SelectAreaWidget
