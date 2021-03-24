ef294a539b92a786b96cc42f93f31ee5b8c1d556
mikesmithlab <mikesmithlab@gmail.com>
2021-03-24 14:55:42 +0000

    Spinbox rounding error bug

--- boilerplate.py
+++ boilerplate.py
@@ -2,19 +2,20 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout
 from PyQt5.QtCore import pyqtSlot
 from qtwidgets.sliders import QCustomSliderDecimal
+from qtwidgets.spinbox import QSteppedSpinBoxDecimal
 
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         
-        self.slider = QCustomSliderDecimal(min_=2, max_=10, step_=0.05,value_=5,label=True, spinbox=True)
+        self.slider = QCustomSliderDecimal(min_=2, max_=10, step_=0.1,value_=5,label=True, spinbox=False)
         self.setCentralWidget(self.slider)
-        self.slider.valueChanged.connect(self.change)
+        #self.slider.valueChanged.connect(self.change)
         
-    @pyqtSlot(float)
-    def change(self, value):
-        print(value)
+    #@pyqtSlot(float)
+    #def change(self, value):
+    #    print(value)
 
 
 
